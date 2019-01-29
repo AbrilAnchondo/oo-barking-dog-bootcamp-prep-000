386ee7785231523dd178da7228ef187e80cddc7b
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-01-29 04:23:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,24 @@
 # Your code goes here!
 class Dog 
   
-
+ def initialize(name, bark)
+   @name = name
+   @bark = bark
+ end
+ 
+ def name
+   @name = name
+ end
+ 
+ def bark
+   @bark = bark
+ end
+ 
+ def bark
+   @bark
+ end
+ 
+ def name
+   @name
+ end 
 end 
